7d85887375d236a680197440db63c9a94c50e074
Gold-Book <bookgold47@gmail.com>
2021-07-17 20:31:39 +0900

    connect pexels search api

--- app/src/main/java/com/gold/book/pexelsploer/MainActivity.kt
+++ app/src/main/java/com/gold/book/pexelsploer/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.gold.book.pexelsploer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.lifecycleScope
+import com.gold.book.pexelsploer.data.repository.PicturesRepository
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.test).setOnClickListener {
+            lifecycleScope.launch {
+                val response = PicturesRepository().search("sky")
+                Timber.tag("test").d(response.toString())
+            }
+        }
     }
 }

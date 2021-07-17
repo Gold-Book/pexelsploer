3592d028e99193b689a7e3c5c16cd6d2930c698c
Gold-Book <bookgold47@gmail.com>
2021-07-17 21:47:26 +0900

    add glide module

---
+++ app/src/main/java/com/gold/book/pexelsploer/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.gold.book.pexelsploer
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule: AppGlideModule()

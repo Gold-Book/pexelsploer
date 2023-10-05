3aee6458ef3cce246ddf3a195d7fe86d48849ad1
Gold-Book <bookgold47@gmail.com>
2023-10-05 19:48:09 +0900

    test

--- app/src/main/java/com/gold/book/pexelsploer/ui/utils/FullScreenUtils.kt
+++ app/src/main/java/com/gold/book/pexelsploer/ui/utils/FullScreenUtils.kt
@@ -11,15 +11,15 @@ object FullScreenUtils {
     // see: https://developer.android.com/training/system-ui/immersive.html
     fun hideSystemUi(window: Window) {
         if (Build.VERSION.SDK_INT < R) {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                    // Set the content to appear under the system bars so that the
-                    // content doesn't resize when the system bars hide and show.
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    // Hide the nav bar and status bar
-                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+//                    // Set the content to appear under the system bars so that the
+//                    // content doesn't resize when the system bars hide and show.
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    // Hide the nav bar and status bar
+//                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    )
         } else {
             val hideTypes = WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars()
 
